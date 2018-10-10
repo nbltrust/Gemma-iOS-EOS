4378635d9e5db6203fb6838354953014d7f666be
koofrank <koofranker@gmail.com>
2018-10-10 12:08:06 +0800

    init EOS project

---
+++ Gemma-EOS/Gemma_EOS.h
@@ -0,0 +1,19 @@
+//
+//  Gemma_EOS.h
+//  Gemma-EOS
+//
+//  Created by koofrank on 2018/10/10.
+//  Copyright © 2018 com.nbltrustdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gemma_EOS.
+FOUNDATION_EXPORT double Gemma_EOSVersionNumber;
+
+//! Project version string for Gemma_EOS.
+FOUNDATION_EXPORT const unsigned char Gemma_EOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gemma_EOS/PublicHeader.h>
+
+
